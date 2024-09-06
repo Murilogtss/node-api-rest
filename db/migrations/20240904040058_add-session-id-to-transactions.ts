98b7ea819238206e5ee573d48803b109e8fa2116
murilogtss <murilogtss123@gmail.com>
2024-09-06 00:37:56 -0300

    feat: add transaction POST route

--- db/migrations/20240904040058_add-session-id-to-transactions.ts
+++ db/migrations/20240904040058_add-session-id-to-transactions.ts
@@ -2,12 +2,14 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
-    table.uuid('session_id').notNullable().after('id').index()
+    table.uuid('session_id').nullable().after('id').index()
+    table.enum('type', ['credit', 'debit']).notNullable()
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
     table.dropColumn('session_id')
+    table.dropColumn('type')
   })
 }
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,27 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transaction = await knex('transactions').select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      type,
+    })
+
+    return reply.status(201).send()
   })
 }
