2c7dac697e596014165d2b27c27fc4a145a9f8cd
murilogtss <murilogtss123@gmail.com>
2024-09-16 21:53:49 -0300

    feat: implement cookie middleware for request handling

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,34 +2,49 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { string, z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', {
-        as: 'amount',
-      })
-      .first()
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies
 
-    return { summary }
-  })
-
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select('*')
 
     return { transactions }
   })
 
-  app.get('/:id', async (req) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies
+
     const getTransactionParamsSchema = z.object({
       id: string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(req.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
 
     return { transaction }
+  })
+
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies
+
+    const summary = await knex('transactions')
+      .where('session_id', sessionId)
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+
+    return { summary }
   })
 
   app.post('/', async (request, reply) => {
