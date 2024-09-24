04f47a5d8be569b7cbc963c483ab987f2facae9c
murilogtss <murilogtss123@gmail.com>
2024-09-23 22:36:38 -0300

    test: add test for listing all transactions

---
+++ test/transactions.spec.ts
@@ -0,0 +1,47 @@
+import { it, expect, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      type: 'credit',
+      amount: 5000,
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        type: 'credit',
+        amount: 5000,
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      }),
+    ])
+  })
+})
