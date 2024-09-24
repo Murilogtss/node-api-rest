9910b27b18a382e760612d716896e1c596f56a33
murilogtss <murilogtss123@gmail.com>
2024-09-23 22:17:30 -0300

    feat: add test for creating a transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from 'fastify'
+import type { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { string, z } from 'zod'
 import { randomUUID } from 'node:crypto'
